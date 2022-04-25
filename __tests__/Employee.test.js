3b6d9e194a6494489906b989df5b54476ce31bcc
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-25 06:18:06 -0500

    Test for Employee created

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,9 @@
+const Employee = require("../lib/Employee");
+
+test("Creates a Employee", () => {
+  const newEmployee = new Employee("Bob Smith", 1, "bobsmith@yahoo.com");
+  expect(newEmployee.name).toBe("Bob Smith");
+  expect(newEmployee.id).toBe(1);
+  expect(newEmployee.email).toBe("bobsmith@yahoo.com");
+});
+
