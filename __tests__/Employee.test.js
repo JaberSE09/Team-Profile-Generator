c60d80fc8fbee4d63666e2897db09db5b393ed30
Sajid Jaber <sajidjaber@yahoo.com>
2022-04-28 22:50:22 -0500

    setup the input file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,14 @@
 const Employee = require("../lib/Employee");
 
+
+
+
+
 test("Creates a Employee", () => {
   const newEmployee = new Employee("Bob Smith", 1, "bobsmith@yahoo.com");
   expect(newEmployee.name).toBe("Bob Smith");
   expect(newEmployee.id).toBe(1);
   expect(newEmployee.email).toBe("bobsmith@yahoo.com");
 });
+
 
